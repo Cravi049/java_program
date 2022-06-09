a009477626acccfb0076f03c6139292101836413
Ravinshu Chauhan <69211328+Cravi049@users.noreply.github.com>
2022-06-09 14:22:01 +0530

    first program

---
+++ Main.java
@@ -0,0 +1,50 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	               // Student class marksheet
+
+        String name;
+
+        int roll_num;
+
+        int sub1,sub2,sub3,sub4,sub5,per;
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter your Name: ");
+        name=sc.nextLine();
+
+        System.out.println("Enter your Roll no: ");
+        roll_num=sc.nextInt();
+
+        System.out.println("Marks in English:");
+        sub1=sc.nextInt();
+
+        System.out.println("Marks in Hindi:");
+        sub2=sc.nextInt();
+
+        System.out.println("Marks in Maths:");
+        sub3=sc.nextInt();
+
+        System.out.println("Marks in Science:");
+        sub4=sc.nextInt();
+
+        System.out.println("Marks in Drawing:");
+        sub5=sc.nextInt();
+
+
+
+                //         Percentage
+
+        per=((sub1+sub2+sub3+sub4+sub5)*100)/500;
+
+        System.out.println("Your Name Is: "+ name);
+        System.out.println("Your Roll no Is; "+ roll_num);
+        System.out.println("Percentage of 5 subjects are: "+ per);
+
+    }
+}
